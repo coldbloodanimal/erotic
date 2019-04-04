bb7a81dcd78e7f2d39b07d8b087268b4042d0ec0
zhaowei <dang7758@126.com>
2019-04-04 20:13:27 +0800

    地址

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+//    https://help.aliyun.com/document_detail/31927.html?spm=a2c4g.11186623.4.6.f1d74d32ZCIWz3
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
